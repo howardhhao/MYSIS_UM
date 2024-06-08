6f8b87b1c3c9341a22ff6ef4d88101c5069f972b
howardhhao <wongwenhao19@gmail.com>
2024-06-09 02:12:19 +0800

    minor changes on about us

--- src/views/AboutUsPage.jsx
+++ src/views/AboutUsPage.jsx
@@ -5,6 +5,7 @@ import NavBarMysis from '../components/NavBarMysis';
 import Footer from '../components/Footer';
 
 function AboutUsPage() {
+  // implement pagination
 
   return (
     <>
@@ -17,44 +18,6 @@ function AboutUsPage() {
           <div className='intro-btn-container'>
           </div>
         </div>
-
-        <div class="aboutUs2-container">
-          <p className='aboutUs2-title'>Our History</p>
-
-          <div className='animation-container'>
-            <div className="vertical-line">
-              <div className='phase-1-year'>1905</div>
-            </div>
-            <div className='phase-1-title'>King Edward VII College of Medicine</div>
-          </div>
-          
-          <div className='animation-container'>
-            <div className="vertical-line-2"></div>
-            <div className='phase-2-year'>1949</div>
-            <div className='phase-2-title'>Raffles College</div>
-          </div>
-
-          <div className='animation-container'>
-            <div className="vertical-line-3"></div>
-            <div className='phase-3-year'>1962</div>
-            <div className='phase-3-title'>University of Malaya</div>
-          </div>
-         
-
-
-        </div>
-
-        <div class="aboutUs3-container">
-          <p className='aboutUs1-title'></p>
-          <div className='intro-btn-container'>
-          </div>
-        </div>
-
-        
-
-        
-
-        
 
        <Footer/>
 
